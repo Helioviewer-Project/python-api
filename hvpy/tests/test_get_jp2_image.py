8151856ffde18428a35e3f6e460dd2438b29ad73
Akash Verma <akashzsh08@gmail.com>
2022-06-15 20:51:16 +0530

    Update hvpy/tests/test_get_jp2_image.py
    Co-authored-by: Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>

--- hvpy/tests/test_get_jp2_image.py
+++ hvpy/tests/test_get_jp2_image.py
@@ -11,7 +11,7 @@ def test_get_jp2_image():
     DATE = datetime(2022, 1, 1, 23, 59, 59)
 
     input = {"date": DATE, "sourceId": 14}
-    input = getJP2ImageInputParameters(input)
+    input = getJP2ImageInputParameters(**input)
 
     output = OutputType.String
     r = execute_api_call(url=URL, input_parameters=input.dict(), output_type=output)
