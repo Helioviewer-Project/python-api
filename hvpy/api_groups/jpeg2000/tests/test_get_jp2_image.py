95cba5a26f40c689fcb4447bcb9509c9b84bf101
akash5100 <shivamvermashs@gmail.com>
2022-06-25 15:26:04 +0530

    Test for value error

--- hvpy/api_groups/jpeg2000/tests/test_get_jp2_image.py
+++ hvpy/api_groups/jpeg2000/tests/test_get_jp2_image.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 
+import pytest
+
 from hvpy.api_groups.jpeg2000.get_jp2_image import getJP2ImageInputParameters
 from hvpy.core import execute_api_call
 
 
+# Test Response
 def test_str_response():
-    date = datetime(2022, 1, 1, 23, 59, 59)
-    params = {"date": date, "sourceId": 14, "jpip": True, "Json": False}
+    date_obj = datetime(2022, 1, 1, 23, 59, 59)
+    params = {"date": date_obj, "sourceId": 14, "jpip": True, "Json": False}
     params = getJP2ImageInputParameters(**params)
     response = execute_api_call(input_parameters=params)
     assert isinstance(response, str)
@@ -14,8 +17,8 @@ def test_str_response():
 
 
 def test_json_response():
-    date = datetime(2022, 1, 1, 23, 59, 59)
-    params = {"date": date, "sourceId": 14, "jpip": True, "Json": True}
+    date_obj = datetime(2022, 1, 1, 23, 59, 59)
+    params = {"date": date_obj, "sourceId": 14, "jpip": True, "Json": True}
     params = getJP2ImageInputParameters(**params)
     response = execute_api_call(input_parameters=params)
     assert isinstance(response, dict)
@@ -24,24 +27,36 @@ def test_json_response():
 
 
 def test_raw_response():
-    date = datetime(2022, 1, 1, 23, 59, 59)
-    params = {"date": date, "sourceId": 14, "jpip": False, "Json": False}
+    date_obj = datetime(2022, 1, 1, 23, 59, 59)
+    params = {"date": date_obj, "sourceId": 14, "jpip": False, "Json": False}
     params = getJP2ImageInputParameters(**params)
     response = execute_api_call(input_parameters=params)
     assert isinstance(response, bytes)
 
 
 def test_raw_response_with_json():
-    date = datetime(2022, 1, 1, 23, 59, 59)
-    params = {"date": date, "sourceId": 14, "jpip": False, "Json": True}
+    date_obj = datetime(2022, 1, 1, 23, 59, 59)
+    params = {"date": date_obj, "sourceId": 14, "jpip": False, "Json": True}
     params = getJP2ImageInputParameters(**params)
     response = execute_api_call(input_parameters=params)
     assert isinstance(response, bytes)
 
 
 def test_default_response():
-    date = datetime(2022, 1, 1, 23, 59, 59)
-    params = {"date": date, "sourceId": 14}
+    date_obj = datetime(2022, 1, 1, 23, 59, 59)
+    params = {"date": date_obj, "sourceId": 14}
     params = getJP2ImageInputParameters(**params)
     response = execute_api_call(input_parameters=params)
     assert isinstance(response, bytes)
+
+
+# Test error
+def test_error_handling():
+    params = {"sourceId": 14, "jpip": True, "Json": True}
+    with pytest.raises(ValueError):
+        params = getJP2ImageInputParameters(**params)
+
+    date_obj = datetime(2022, 1, 1, 23, 59, 59)
+    params = {"date": date_obj, "jpip": True, "Json": False}
+    with pytest.raises(ValueError):
+        params = getJP2ImageInputParameters(**params)
