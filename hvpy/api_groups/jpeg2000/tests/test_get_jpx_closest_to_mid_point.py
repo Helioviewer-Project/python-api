021d4ebc85e090b5ed96090010bddc5e175e1d3b
akash5100 <shivamvermashs@gmail.com>
2022-07-05 13:37:42 +0530

    Split Parameterized to multiple tests

--- hvpy/api_groups/jpeg2000/tests/test_get_jpx_closest_to_mid_point.py
+++ hvpy/api_groups/jpeg2000/tests/test_get_jpx_closest_to_mid_point.py
@@ -10,36 +10,37 @@ startTimes = [datetime(2014, 1, 1, 0, 0, 0), datetime(2014, 1, 1, 2, 3, 3), date
 endTimes = [datetime(2014, 1, 1, 0, 45, 0), datetime(2014, 1, 1, 2, 33, 3), datetime(2014, 1, 1, 4, 54, 5)]
 
 
-@pytest.mark.parametrize(
-    "startTimes, endTimes, sourceId, linked, verbose, jpip",
-    [
-        (startTimes, endTimes, 14, False, False, False),
-        (startTimes, endTimes, 14, False, False, True),
-        (startTimes, endTimes, 14, False, True, True),
-        (startTimes, endTimes, 14, False, True, False),
-    ],
-)
-def test_getJPXClosestToMidPointInputParameters(startTimes, endTimes, sourceId, linked, verbose, jpip):
+def test_raw_response():
     params = getJPXClosestToMidPointInputParameters(
-        startTimes=startTimes,
-        endTimes=endTimes,
-        sourceId=sourceId,
-        linked=linked,
-        verbose=verbose,
-        jpip=jpip,
+        startTimes=startTimes, endTimes=endTimes, sourceId=14, linked=False, verbose=False, jpip=False
     )
     response = execute_api_call(input_parameters=params)
-    if not jpip and not verbose:
-        assert isinstance(response, bytes)
-    elif jpip and not verbose:
-        assert isinstance(response, str)
-        assert response.startswith("jpip://")
-    elif jpip and verbose:
-        assert isinstance(response, dict)
-        assert response["uri"].startswith("jpip://")
-    elif not jpip and verbose:
-        assert isinstance(response, dict)
-        assert response["uri"].startswith("https://")
+    assert isinstance(response, bytes)
+
+
+def test_str_response():
+    params = getJPXClosestToMidPointInputParameters(
+        startTimes=startTimes, endTimes=endTimes, sourceId=14, linked=False, verbose=False, jpip=True
+    )
+    response = execute_api_call(input_parameters=params)
+    assert isinstance(response, str)
+    assert response.startswith("jpip://")
+
+
+def test_json_response():
+    params = getJPXClosestToMidPointInputParameters(
+        startTimes=startTimes, endTimes=endTimes, sourceId=14, linked=False, verbose=True, jpip=True
+    )
+    response = execute_api_call(input_parameters=params)
+    assert isinstance(response, dict)
+    assert response["uri"].startswith("jpip://")
+
+    params = getJPXClosestToMidPointInputParameters(
+        startTimes=startTimes, endTimes=endTimes, sourceId=14, linked=False, verbose=True, jpip=False
+    )
+    response = execute_api_call(input_parameters=params)
+    assert isinstance(response, dict)
+    assert response["uri"].startswith("https://")
 
 
 def test_error_handling():
