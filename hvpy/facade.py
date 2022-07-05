d9ddc9fb410aba39e37d7137058abe79882d7245
akash5100 <shivamvermashs@gmail.com>
2022-07-05 20:59:30 +0530

    Add return types to decorators and functions

--- hvpy/facade.py
+++ hvpy/facade.py
@@ -1,12 +1,13 @@
+from typing import Any, Dict, Union, Callable
 from datetime import datetime
 
 from hvpy.core import execute_api_call
-from hvpy.parameters import getJP2ImageInputParameters
+from hvpy.parameters import *
 
 __all__ = ["getJP2Image"]
 
 
-def _copy_docstring(input_class):
+def _copy_docstring(input_class) -> Callable[[Any], Any]:
     def decorator(func):
         func.__doc__ = (
             "\n".join(func.__doc__.split("\n")[1:3])
@@ -24,12 +25,12 @@ def getJP2Image(
     sourceId: int,
     jpip: bool = False,
     json: bool = False,
-):
+) -> Union[bytes, str, Dict[str, Any]]:
     """
     Retrieve a JP2000 image from the helioviewer.org API.
 
-    Example:
-
+    Examples:
+        >>> from hvpy import getJP2Image
         >>> getJP2Image(date=datetime(2019,1,1), sourceId=1, jpip=True)
         'jpip://helioviewer.org:8090/EIT/2013/08/07/195/2013_08_07__01_13_50_146__SOHO_EIT_EIT_195.jp2'
     """
