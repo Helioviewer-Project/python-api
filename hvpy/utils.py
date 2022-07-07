acaa047a9ad0e3f584feabddec847c1adc438841
akash5100 <shivamvermashs@gmail.com>
2022-07-07 21:52:31 +0530

    fix the error in the docstring

--- hvpy/utils.py
+++ hvpy/utils.py
@@ -5,9 +5,10 @@ __all__ = ["add_shared_docstring"]
 
 def add_shared_docstring(input_class) -> Callable[[Any], Any]:
     def decorator(func):
-        split_doc = input_class.__doc__.split("{Shared}")
-        func.__doc__ = func.__doc__.replace("{Insert}", split_doc[1])
-        input_class.__doc__ = input_class.__doc__.replace("{Shared}", "")
+        if "{Shared}" in input_class.__doc__:
+            split_doc = input_class.__doc__.split("{Shared}")
+            func.__doc__ = func.__doc__.replace("{Insert}", split_doc[1])
+            input_class.__doc__ = input_class.__doc__.replace("{Shared}", "")
         return func
 
     return decorator
