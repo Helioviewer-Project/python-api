f300e1ca80c2b2904e01663ba3479a0585e73f96
akash5100 <shivamvermashs@gmail.com>
2022-07-11 15:14:47 +0530

    Refactor tests

--- hvpy/api_groups/jpeg2000/tests/test_get_jp2_header.py
+++ hvpy/api_groups/jpeg2000/tests/test_get_jp2_header.py
@@ -1,26 +1,23 @@
 import pytest
-from pydantic import ValidationError
 
 from hvpy.api_groups.jpeg2000.get_jp2_header import getJP2HeaderInputParameters
-from hvpy.core import execute_api_call
+from hvpy.facade import getJP2Header
 
 
 def test_getJP2HeaderInputParameters():
-    params = getJP2HeaderInputParameters(id=9838343)
-    response = execute_api_call(input_parameters=params)
+    response = getJP2Header(id=9838343)
     assert isinstance(response, str)
     assert response.startswith("<?xml")
     assert response.endswith("</meta>")
 
-    params = getJP2HeaderInputParameters(id=9838343, callback="my_callback")
-    response = execute_api_call(input_parameters=params)
+    response = getJP2Header(id=9838343, callback="my_callback")
     assert isinstance(response, str)
     assert response.startswith("my_callback(")
 
 
 def test_error_handling():
-    with pytest.raises(ValidationError, match="getJP2HeaderInputParameters\nid\n  field required"):
-        getJP2HeaderInputParameters(callback="my_callback")
+    with pytest.raises(TypeError, match="missing 1 required positional argument: 'id'"):
+        getJP2Header(callback="my_callback")
 
 
 def test_url_property():
