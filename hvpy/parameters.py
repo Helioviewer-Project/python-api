809387a24a2ccb9ae7eed1527f10065ace4aaff8
akash5100 <shivamvermashs@gmail.com>
2022-07-11 13:29:25 +0530

    bring all endpoints to facade

--- hvpy/parameters.py
+++ hvpy/parameters.py
@@ -1,3 +1,13 @@
+from hvpy.api_groups.jpeg2000.get_jp2_header import getJP2HeaderInputParameters
 from hvpy.api_groups.jpeg2000.get_jp2_image import getJP2ImageInputParameters
+from hvpy.api_groups.jpeg2000.get_jpx import getJPXInputParameters
+from hvpy.api_groups.jpeg2000.get_jpx_closest_to_mid_point import getJPXClosestToMidPointInputParameters
+from hvpy.api_groups.jpeg2000.get_status import getStatusInputParameters
 
-__all__ = ["getJP2ImageInputParameters"]
+__all__ = [
+    "getJP2ImageInputParameters",
+    "getJP2HeaderInputParameters",
+    "getJPXClosestToMidPointInputParameters",
+    "getJPXInputParameters",
+    "getStatusInputParameters",
+]
