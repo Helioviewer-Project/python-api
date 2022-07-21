9a2ba5ec7977aa45732c4acfbcd4564972edc872
akash5100 <shivamvermashs@gmail.com>
2022-07-21 20:26:28 +0530

    test for queueMovie

--- hvpy/api_groups/movies/tests/test_queue_movie.py
+++ hvpy/api_groups/movies/tests/test_queue_movie.py
@@ -1,15 +1,37 @@
 from datetime import datetime
 
+import pytest
+
 from hvpy import queueMovie
+from hvpy.api_groups.movies.queue_movie import queueMovieInputParameters
 
 
 def test_queue():
     response = queueMovie(
-        startTime=datetime(2022, 7, 12, 12, 12, 12),
-        endTime=datetime(2022, 7, 13, 12, 12, 12),
+        startTime=datetime(2022, 7, 20, 12, 12, 12),
+        endTime=datetime(2022, 7, 21, 12, 12, 12),
         layers="[12,7,22],[13,7,22]",
         events="[AR,HMI_HARP;SPoCA,1],[CH,all,1]",
         eventsLabels=False,
-        imageScale=21.04,
+        imageScale=1,
     )
-    print(response)
+    assert response["id"] is not None
+    assert response["eta"] is not None
+    assert response["queue"] is not None
+    assert response["token"] is not None
+
+
+def test_error_handling():
+    with pytest.raises(TypeError, match="missing 1 required positional argument: 'imageScale'"):
+        response = queueMovie(
+            startTime=datetime(2022, 7, 20, 12, 12, 12),
+            endTime=datetime(2022, 7, 21, 12, 12, 12),
+            layers="[12,7,22],[13,7,22]",
+            events="[AR,HMI_HARP;SPoCA,1],[CH,all,1]",
+            eventsLabels=False,
+        )
+
+
+def test_url_property():
+    params = queueMovieInputParameters(id=9838343)
+    assert params.url == "https://api.helioviewer.org/v2/queueMovie/"
