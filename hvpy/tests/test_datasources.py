d14f99b2bb26671fe8d01a21f0f2ec4b4b092b28
akash5100 <shivamvermashs@gmail.com>
2022-08-20 21:46:33 +0530

    Add docstring and clean some lines

--- hvpy/tests/test_datasources.py
+++ hvpy/tests/test_datasources.py
@@ -1,24 +1,34 @@
+from typing import Any
+
 from hvpy import DataSources, getDataSources
 
-sids = list()
 
+def find_nested_keys(obj: dict, key: Any, out: list):
+    """
+    Recursively walks through a nested dictionary and extracts values
+    associated with the given key.
 
-def find_key(obj, key):
+    obj - dictionary with nested dictionaries
+    key - Key to find within the nest
+    out - values found are appended to this list
+    """
     if key in obj:
-        sids.append(obj[key])
+        out.append(obj[key])
     else:
         for k, v in obj.items():
             if isinstance(v, dict):
-                find_key(v, key)
+                find_nested_keys(v, key, out)
             else:
                 continue
 
 
 def test_datasources():
+    source_id_list = list()
     enum_value_list = [s_id.value for s_id in DataSources]
-    res = getDataSources()
 
-    find_key(res, "sourceId")
+    response = getDataSources()
 
-    for sid in sids:
-        assert sid in enum_value_list
+    find_nested_keys(response, "sourceId", source_id_list)
+
+    for source_id in source_id_list:
+        assert source_id in enum_value_list
