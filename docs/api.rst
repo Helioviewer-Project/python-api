a12f594540ec986ea04a4499c8adf4b8fa5f4b8f
Akash Verma <akashzsh08@gmail.com>
2022-08-20 22:26:16 +0530

    Enum for Datasources (#61)
    Co-authored-by: Nabil Freij <nabil.freij@gmail.com>

--- docs/api.rst
+++ docs/api.rst
@@ -3,10 +3,15 @@ API Reference
 *************
 
 .. automodapi:: hvpy
+   :no-inheritance-diagram:
 
 .. automodapi:: hvpy.core
 
 .. automodapi:: hvpy.io
+   :no-inheritance-diagram:
 
 .. automodapi:: hvpy.parameters
    :allowed-package-names: hvpy.api_groups
+
+.. automodapi:: hvpy.datasources
+   :no-inheritance-diagram:
---
+++ hvpy/tests/test_datasources.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from hvpy import DataSources, getDataSources
+
+
+def find_nested_keys(obj: dict, key: Any, out: list):
+    """
+    Recursively walks through a nested dictionary and extracts values
+    associated with the given key.
+
+    obj - dictionary with nested dictionaries
+    key - Key to find within the nest
+    out - values found are appended to this list
+    """
+    if key in obj:
+        out.append(obj[key])
+    else:
+        for k, v in obj.items():
+            if isinstance(v, dict):
+                find_nested_keys(v, key, out)
+            else:
+                continue
+
+
+def test_datasources():
+    source_ids = []
+    enum_values = [s_id.value for s_id in DataSources]
+    response = getDataSources()
+    find_nested_keys(response, "sourceId", source_ids)
+    for source_id in source_ids:
+        assert source_id in enum_values
