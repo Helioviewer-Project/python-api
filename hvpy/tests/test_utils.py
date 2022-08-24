d12c676955cda24e72fc3631f414227d07848124
akash5100 <shivamvermashs@gmail.com>
2022-08-24 20:53:14 +0530

    add create event string function

--- hvpy/tests/test_utils.py
+++ hvpy/tests/test_utils.py
@@ -1,18 +1,22 @@
 import pytest
 
-from hvpy import DataSource
-from hvpy.utils import _create_layer_string, _to_datasource, create_layers
+from hvpy import DataSource, EventType
+from hvpy.utils import (
+    _create_events_string,
+    _create_layer_string,
+    _to_datasource,
+    _to_event_type,
+    create_events,
+    create_layers,
+)
 
 
 def test_create_layers():
     assert create_layers([(9, 100), (11, 50)]) == "[9,1,100],[11,1,50]"
-
     with pytest.raises(ValueError, match="999 is not a valid DataSource"):
         create_layers([(9, 100), (999, 100)])
-
     with pytest.raises(ValueError, match="must be between 0 and 100"):
         create_layers([(9, 101), (14, 100)])
-
     with pytest.raises(ValueError, match="must be between 0 and 100"):
         create_layers([(9, 100), (14, -1)])
 
@@ -30,3 +34,18 @@ def test_create_layers_string():
         _create_layer_string(DataSource.AIA_131, 101)
     with pytest.raises(ValueError, match="must be between 0 and 100"):
         _create_layer_string(DataSource.AIA_131, -1)
+
+
+def test_to_event_type():
+    assert _to_event_type("AR") == EventType.ACTIVE_REGION
+    assert _to_event_type(EventType.ACTIVE_REGION) == EventType.ACTIVE_REGION
+
+
+def test_create_events():
+    assert create_events([(EventType.ACTIVE_REGION), (EventType.ERUPTION)]) == "[AR,all,1],[ER,all,1]"
+    with pytest.raises(ValueError, match="STRING is not a valid EventType"):
+        create_events([(EventType.ACTIVE_REGION), (EventType.ERUPTION), ("STRING")])
+
+
+def test_create_events_string():
+    assert _create_events_string(EventType.ACTIVE_REGION) == "[AR,all,1]"
--- hvpy/utils.py
+++ hvpy/utils.py
@@ -2,11 +2,13 @@ from typing import Any, Union, Callable
 from datetime import datetime
 
 from hvpy.datasource import DataSource
+from hvpy.event import EventType
 
 __all__ = [
     "convert_date_to_isoformat",
     "convert_date_to_unix",
     "create_layers",
+    "create_events",
 ]
 
 
@@ -75,3 +77,40 @@ def create_layers(layer: list) -> str:
     '[3,1,50],[10,1,50]'
     """
     return ",".join([_create_layer_string(_to_datasource(s), o) for s, o in layer])
+
+
+def _to_event_type(val: Union[str, EventType]) -> EventType:
+    """
+    Validates the input and converts it to a EventType enum.
+    """
+    if isinstance(val, str) and val in [x.value for x in EventType]:
+        return EventType(val)
+    elif isinstance(val, EventType):
+        return val
+    else:
+        raise ValueError(f"{val} is not a valid EventType")
+
+
+def _create_events_string(event_type: EventType) -> str:
+    """
+    Generates a string of the form "[event_type,all,1]" for a event.
+    """
+    return f"[{event_type.value},all,1]"
+
+
+def create_events(event: list) -> str:
+    """
+    Creates a string of events separated by commas.
+
+    Parameters
+    ----------
+    event
+        A list of tuples of the form (``event_type``).
+
+    Examples
+    --------
+    >>> from hvpy import create_event_string, EventType
+    >>> create_event_string([(EventType.ACTIVE_REGION), (EventType.ERUPTION)])
+    '[AR,all,1],[ER,all,1]'
+    """
+    return ",".join([_create_events_string(_to_event_type(e)) for e in event])
