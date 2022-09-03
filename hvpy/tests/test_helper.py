263badd750399cabe0f2e981f38b173035b6ff32
akash5100 <shivamvermashs@gmail.com>
2022-09-03 22:18:06 +0530

    fix failing test

--- hvpy/tests/test_helper.py
+++ hvpy/tests/test_helper.py
@@ -14,3 +14,29 @@ def test_create_movie(start_time, end_time, tmp_path):
         filename=f1,
     )
     assert result.exists()
+    assert result == tmp_path / "movie.mp4"
+
+    result = createMovie(
+        startTime=start_time,
+        endTime=end_time,
+        layers=create_layers([(DataSource.AIA_171, 100)]),
+        events=create_events(["AR"]),
+        eventsLabels=True,
+        imageScale=1,
+        filename=f1,
+    )
+    assert result.exists()
+    assert result == tmp_path / "movie(1).mp4"
+
+    result = createMovie(
+        startTime=start_time,
+        endTime=end_time,
+        layers=create_layers([(DataSource.AIA_171, 100)]),
+        events=create_events(["AR"]),
+        eventsLabels=True,
+        imageScale=1,
+        filename=f1,
+        overwrite=True,
+    )
+    assert result.exists()
+    assert result == tmp_path / "movie.mp4"
